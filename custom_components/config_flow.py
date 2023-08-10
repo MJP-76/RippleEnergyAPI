2f5072e995bccb8926ec14e04d02c93039ee78ac
matthewjporter <matthew@porterfamily.co.za>
2023-08-10 17:10:10 +0100

    Create config_flow.py

---
+++ custom_components/config_flow.py
@@ -0,0 +1,29 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class RippleEnergyConfigFlow(config_entries.ConfigFlow, domain=ripple_energy):
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            # Validate the API key here
+            if valid_api_key(user_input['api_key']):
+                return self.async_create_entry(title="Ripple Energy", data=user_input)
+            else:
+                errors["base"] = "invalid_api_key"
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("api_key"): str,
+            }),
+            errors=errors,
+        )
+
+# You'll need to implement a function to validate the API key
+def valid_api_key(api_key):
+    # Implement your validation logic here
+    return True
+
