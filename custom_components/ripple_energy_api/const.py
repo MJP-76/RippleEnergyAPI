d5aac7d620f5a41c300357c9e29851eed5cd529f
matthewjporter <matthew@porterfamily.co.za>
2023-08-10 18:14:38 +0100

    Update const.py

--- custom_components/ripple_energy_api/const.py
+++ custom_components/ripple_energy_api/const.py
@@ -4,7 +4,3 @@ DOMAIN = "ripple_energy"
 CONFIG_NAME = "Ripple Energy"
 CONFIG_SCAN_INTERVAL = "300"
 CONFIG_API_KEY: "API Key"
-
-DATA_SCHEMA_ACCOUNT = vol.Schema({
-  vol.Required(CONFIG_API_KEY): str
-})
