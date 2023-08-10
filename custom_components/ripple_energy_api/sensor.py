269597722458b094d3a67653088ad8dc2332f027
matthewjporter <matthew@porterfamily.co.za>
2023-08-10 21:38:03 +0100

    Update sensor.py

--- custom_components/ripple_energy_api/sensor.py
+++ custom_components/ripple_energy_api/sensor.py
@@ -4,7 +4,7 @@ def get_data(api_key):
     headers = {
         "Authorization": f"Bearer {api_key}"
     }
-    response = requests.get("https://rippleenergy.com/rest/member_data/", headers=headers)
+    response = requests.get("", headers=headers)
     return response.json()
 
 from homeassistant.helpers.entity import Entity
