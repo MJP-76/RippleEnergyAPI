9dcb0264b8a2021fec1aedcc6c9542a1cc993446
matthewjporter <matthew@porterfamily.co.za>
2023-08-11 09:38:52 +0100

    Update sensor.py

--- custom_components/ripple_energy_api/sensor.py
+++ custom_components/ripple_energy_api/sensor.py
@@ -1,40 +1,81 @@
-import requests
-import config_flow
 import json
-
+import requests
+from homeassistant.helpers import config_entry_flow
 from homeassistant.helpers.entity import Entity
-from homeassistant.components.sensor import SensorEntityDescription
 
-def get_data(api_key):
-    headers = {
-        "Authorization": f"Bearer {api_key}"
-    }
-    response = requests.get("https://rippleenergy.com/rest/member_data/", headers=headers)
-    return response.json()
+DOMAIN = "ripple_energy"
+PLATFORMS = ["sensor"]
 
+# Example of API endpoint and headers
+API_ENDPOINT = "https://rippleenergy.com/rest/member_data/"
+HEADERS = {
+    "Authorization": "Bearer YOUR_API_TOKEN",
+    "Content-Type": "application/json"
+}
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    api_key = config.get("api_key")
-    data = await hass.async_add_executor_job(get_data, api_key)
-    
+async def async_setup_entry(hass, entry, async_add_entities):
+    api_data = await fetch_data_from_api()
     sensors = []
-    for item in data:
-        sensors.append(RippleEnergySensor(item))
-    
+
+    for device in api_data.get("devices", []):
+        sensors.append(RippleEnergySensor(device))
+
     async_add_entities(sensors, True)
 
-class RippleEnergySensor(SensorEntity):
-    def __init__(self, data):
-        self._data = data
+async def fetch_data_from_api():
+    try:
+        response = requests.get(API_ENDPOINT, headers=HEADERS)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as error:
+        print("Error fetching data from API:", error)
+        return {}
+
+class RippleEnergySensor(Entity):
+    def __init__(self, device_info):
+        self._device_info = device_info
         self._state = None
 
     @property
     def name(self):
-        return self._data["generation_assets[*].name"]
+        return f"Ripple Energy {self._device_info['name']}"
+
+    @property
+    def unique_id(self):
+        return self._device_info["id"]
 
     @property
     def state(self):
         return self._state
 
-    def update(self):
-        self._state = self._data["value"]
+    @property
+    def unit_of_measurement(self):
+        return "kWh"
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._device_info["id"])},
+            "name": self._device_info["name"],
+            "manufacturer": "Ripple Energy",
+        }
+
+    async def async_update(self):
+        api_data = await fetch_data_from_api()
+        device = next((d for d in api_data.get("devices", []) if d["id"] == self.unique_id), None)
+        if device:
+            self._state = device.get("energy_consumed", None)
+
+async def async_step_import(hass, config):
+    return await async_step_user(hass, config)
+
+config_entry_flow.register_webhook_flow(
+    DOMAIN,
+    "Ripple Energy",
+    {
+        "config_entries": config_entry_flow.HANDLERS,
+        "discovery": "discovery",
+        "import": async_step_import,
+    },
+)
+
