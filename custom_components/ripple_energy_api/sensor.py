5c60e90ac124606256a5a442e6c138e7feafb64d
matthewjporter <matthew@porterfamily.co.za>
2023-08-11 09:49:22 +0100

    Update sensor.py

--- custom_components/ripple_energy_api/sensor.py
+++ custom_components/ripple_energy_api/sensor.py
@@ -1,81 +1,45 @@
-import json
 import requests
-from homeassistant.helpers import config_entry_flow
-from homeassistant.helpers.entity import Entity
+import json
 
-DOMAIN = "ripple_energy"
-PLATFORMS = ["sensor"]
+# URL of the Ripple Energy API endpoint
+api_url = "https://rippleenergy.com/rest/member_data/KR8IOQKc_VujCBcL:arCYlJieSW1BmnjIvcdLB_9GPUQncKFgUtO3eGoBoRk"
 
-# Example of API endpoint and headers
-API_ENDPOINT = "https://rippleenergy.com/rest/member_data/"
-HEADERS = {
-    "Authorization": "Bearer YOUR_API_TOKEN",
-    "Content-Type": "application/json"
-}
+# Make an HTTP GET request to the API
+response = requests.get(api_url)
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    api_data = await fetch_data_from_api()
-    sensors = []
-
-    for device in api_data.get("devices", []):
-        sensors.append(RippleEnergySensor(device))
-
-    async_add_entities(sensors, True)
-
-async def fetch_data_from_api():
-    try:
-        response = requests.get(API_ENDPOINT, headers=HEADERS)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as error:
-        print("Error fetching data from API:", error)
-        return {}
-
-class RippleEnergySensor(Entity):
-    def __init__(self, device_info):
-        self._device_info = device_info
-        self._state = None
-
-    @property
-    def name(self):
-        return f"Ripple Energy {self._device_info['name']}"
-
-    @property
-    def unique_id(self):
-        return self._device_info["id"]
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        return "kWh"
-
-    @property
-    def device_info(self):
-        return {
-            "identifiers": {(DOMAIN, self._device_info["id"])},
-            "name": self._device_info["name"],
-            "manufacturer": "Ripple Energy",
-        }
-
-    async def async_update(self):
-        api_data = await fetch_data_from_api()
-        device = next((d for d in api_data.get("devices", []) if d["id"] == self.unique_id), None)
-        if device:
-            self._state = device.get("energy_consumed", None)
-
-async def async_step_import(hass, config):
-    return await async_step_user(hass, config)
-
-config_entry_flow.register_webhook_flow(
-    DOMAIN,
-    "Ripple Energy",
-    {
-        "config_entries": config_entry_flow.HANDLERS,
-        "discovery": "discovery",
-        "import": async_step_import,
-    },
-)
-
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    
+    # Extract relevant information from the JSON response
+    energy_production = data.get("energy_production", 0)  # Replace with actual key
+    
+    # Assuming you want to create a sensor for energy production
+    sensor_config = {
+        "name": "Ripple Energy Production",
+        "state_class": "measurement",
+        "unit_of_measurement": "kWh",
+        "state": energy_production
+    }
+    
+    # Convert the sensor configuration to JSON
+    sensor_json = json.dumps(sensor_config)
+    
+    # URL of Home Assistant API endpoint to create/update sensors
+    ha_api_url = "http://YOUR_HA_IP:8123/api/states/sensor.ripple_energy_production"  # Replace with your Home Assistant IP
+    
+    # Headers for the Home Assistant API request
+    headers = {
+        "Authorization": "Bearer YOUR_HA_ACCESS_TOKEN",  # Replace with your Home Assistant access token
+        "Content-Type": "application/json"
+    }
+    
+    # Make a POST request to create/update the sensor in Home Assistant
+    ha_response = requests.post(ha_api_url, headers=headers, data=sensor_json)
+    
+    if ha_response.status_code == 200:
+        print("Sensor created/updated successfully in Home Assistant")
+    else:
+        print("Failed to create/update sensor in Home Assistant")
+else:
+    print("Failed to fetch data from Ripple Energy API")
