754763ef9a58d3bf8b0c2dd9cf082ccf09cd7ba1
matthewjporter <matthew@porterfamily.co.za>
2023-08-11 11:20:51 +0100

    Back to basics

--- custom_components/ripple_energy_api/sensor.py
+++ custom_components/ripple_energy_api/sensor.py
@@ -1,45 +1,32 @@
 import requests
-import json
 
-# URL of the Ripple Energy API endpoint
-api_url = "https://rippleenergy.com/rest/member_data/KR8IOQKc_VujCBcL:arCYlJieSW1BmnjIvcdLB_9GPUQncKFgUtO3eGoBoRk"
-
-# Make an HTTP GET request to the API
-response = requests.get(api_url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
-    
-    # Extract relevant information from the JSON response
-    energy_production = data.get("generation_assets[1].generation.today.generated")  # Replace with actual key
-    
-    # Assuming you want to create a sensor for energy production
-    sensor_config = {
-        "name": "Kirk Hill Generation Today",
-        "state_class": "measurement",
-        "unit_of_measurement": "kWh",
-        "state": energy_production
-    }
-    
-    # Convert the sensor configuration to JSON
-    sensor_json = json.dumps(sensor_config)
-    
-    # URL of Home Assistant API endpoint to create/update sensors
-    ha_api_url = "https://172.16.1.2/api/states/sensor.ripple_energy_generation_today"  # Replace with your Home Assistant IP
-    
-    # Headers for the Home Assistant API request
+def get_data(api_key):
     headers = {
-        "Authorization": "Bearer YOUR_HA_ACCESS_TOKEN",  # Replace with your Home Assistant access token
-        "Content-Type": "application/json"
+        "Authorization": f"Bearer {api_key}"
     }
+    response = requests.get("https://api.rippleenergy.com/data", headers=headers)
+    return response.json()
+
+from homeassistant.helpers.entity import Entity
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    api_key = config.get("api_key")
+    data = await hass.async_add_executor_job(get_data, api_key)
     
-    # Make a POST request to create/update the sensor in Home Assistant
-    ha_response = requests.post(ha_api_url, headers=headers, data=sensor_json)
+    sensors = []
+    for item in data:
+        sensors.append(RippleEnergySensor(item))
     
-    if ha_response.status_code == 200:
-        print("Sensor created/updated successfully in Home Assistant")
-    else:
-        print("Failed to create/update sensor in Home Assistant")
-else:
-    print("Failed to fetch data from Ripple Energy API")
+    async_add_entities(sensors, True)
+
+class RippleEnergySensor(Entity):
+    def __init__(self, data):
+        self._data = data
+        self._state = None
+
+    @property
+    def name(self):
+        return self._data["email"]
+
+    def update(self):
+        self._state = self._data["value"]
